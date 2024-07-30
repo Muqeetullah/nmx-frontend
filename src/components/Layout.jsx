73a5305b2154b7ddb14f480d7dd82be8a9a3c6aa
Muqeet_ullah <muqeet.ullah@codistan.org>
2024-07-30 16:32:10 +0500

    Error Boundtry Functioanity added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,14 @@
+import { ErrorBoundary } from "react-error-boundary";
 import Header from "./Header";
 import React from "react";
+import FallbackScreen from "./ErrorScreen";
 const Layout = ({ children }) => {
   return (
     <div className=" w-[100%]">
       <Header />
-      <div className="flex space-x-2 md:space-x-8">{children}</div>
+      <ErrorBoundary FallbackComponent={FallbackScreen}>
+        <div className="flex space-x-2 md:space-x-8">{children}</div>
+      </ErrorBoundary>
     </div>
   );
 };
